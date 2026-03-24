ecb86b1c91280b686a1a756f43f3b43dc1e09365
fadhil004 <m.fadhil238@gmail.com>
2026-03-24 19:36:20 +0700

    transaction handler

---
+++ rest-api-bank/handler/transfer_handler.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"rest-api-bank/dto"
+	"rest-api-bank/service"
+
+	"github.com/google/uuid"
+)
+
+type TransferHandler struct {
+	Service *service.TransferService
+}
+
+func (h *TransferHandler) Transfer() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var req dto.TransferRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "invalid body", 400)
+			return
+		}
+
+		fromID, err := uuid.Parse(req.FromAccountID)
+		if err != nil {
+			http.Error(w, "invalid from_account_id", 400)
+			return
+		}
+
+		toID, err := uuid.Parse(req.ToAccountID)
+		if err != nil {
+			http.Error(w, "invalid to_account_id", 400)
+			return
+		}
+
+		err = h.Service.Transfer(fromID, toID, req.Amount)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+
+		json.NewEncoder(w).Encode(dto.BaseResponse{
+			Message: "Transfer success",
+		})
+	}
+}
